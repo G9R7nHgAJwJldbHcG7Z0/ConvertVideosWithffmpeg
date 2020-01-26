861db72173f301fc35e39ccabdef5f666bee39ed
G9R7nHgAJwJldbHcG7Z0 <59141742+G9R7nHgAJwJldbHcG7Z0@users.noreply.github.com>
2020-01-26 17:14:51 -0500

    Added PowerShell ConvertVideosWithffmpeg.ps1 file
    Added ConvertVideosWithffmpeg.ps1 code file to directory.

---
+++ ConvertVideosWithffmpeg.ps1
@@ -0,0 +1,15 @@
+﻿#Written by Daniel Kill
+#Modified: 2020-01-07 by Daniel Kill
+Clear-Host
+
+do
+{
+    [string]$dir = Read-Host -Prompt "What path"
+    if (!(Test-Path $dir)) {Write-Warning -Message "*** Invalid Path ***"}
+} until (Test-Path $dir)
+
+Set-Location -Path $dir
+
+[string]$filter = Read-Host -Prompt "What filter"
+
+Get-ChildItem -Path $dir -Filter ("*" + $filter + "*") | ForEach-Object {ffmpeg -i ($PSItem.Name) -threads 4 -vcodec libx264 -crf 28 -acodec aac -f mp4 (($PSItem.BaseName) + "-NEW.mp4")}
